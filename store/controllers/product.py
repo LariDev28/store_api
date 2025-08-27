14d82acdd69adbfd3918deba227958544f5ef3be
Larissa T Freitas <larissa.lafreitas@gmail.com>
2025-08-26 21:24:39 -0300

    Update product.py

--- store/controllers/product.py
+++ store/controllers/product.py
@@ -30,8 +30,12 @@ async def get(
 
 
 @router.get(path="/", status_code=status.HTTP_200_OK)
-async def query(usecase: ProductUsecase = Depends()) -> List[ProductOut]:
-    return await usecase.query()
+async def query(
+    min_price: float = None,
+    max_price: float = None,
+    usecase: ProductUsecase = Depends()
+) -> List[ProductOut]:
+    return await usecase.query(min_price=min_price, max_price=max_price)
 
 
 @router.patch(path="/{id}", status_code=status.HTTP_200_OK)
